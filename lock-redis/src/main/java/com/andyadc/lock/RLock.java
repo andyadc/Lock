6f4c98ebd22e525b3ed88225945f4bd84320e8f2
99BILL\andy.an <andy.an@99bill.com>
2018-07-09 17:01:50 +0800

    Initial projects

---
+++ lock-redis/src/main/java/com/andyadc/lock/RLock.java
@@ -0,0 +1,8 @@
+package com.andyadc.lock;
+
+/**
+ * @author andy.an
+ * @since 2018/7/9
+ */
+public class RLock {
+}
---
+++ lock-redis/src/main/resources/lock.lua
@@ -0,0 +1,12 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: andy.an
+-- Date: 2018/7/9
+-- Time: 16:57
+-- To change this template use File | Settings | File Templates.
+--
+if redis.call('get', KEY[1]) == ARGV[1] then
+    return redis.call('del', KEYS[1])
+else
+    return 0
+end
